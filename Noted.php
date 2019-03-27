3c6f3ecf5ce94d6bb2d5448ddbfbdd5c32e9cadf
ogisetiawan <ogisetiawan21@gmail.com>
2019-03-27 16:29:02 +0700

    upload

---
+++ Noted.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * ! ### INSTALL GIT ###
+ * ? add repositiry: masuk ke dalam directory yang akan di push
+ * * git init 
+ * ? intial git. dan menambhakan folder git (hidden)
+ * ? tambahakan file .gitignore
+ * ? yang berfungsi sebagai file mana saja yang akan diabaikan oleh git dalam souce control
+ */
+
+/**
+ * ! ### SIMBOL GIT VS CODE
+ * * U artinya Untracked adalah file yang belum pernah dimasukkan ke dalam Git (file baru).
+ * * M artinya Modified adalah file yang sudah dimasukkan ke dalam database Git dan sudah pernah dimodifikasi.
+ * * D artinya Deleted adalah file yang dihapus.
+ * * A artinya Added adalah file yang ditambahkan ke dalam mode staged.
+ */
+
+ /**
+  * ! ### STAGGED KE DALAM .GIT ###
+  * ? memasukan file atau perubahan pada resporsity ke dalam .git semestera
+  */
+
+  /**
+   * ! ### WORKING REMOTE REPOSITORY GIT ###
+   * ? buatlah sebuah repository baru pada github.
+   * ? copy url git
+   * ? masukan perintah add origin dan marge url git hasil clone 
+   * * git remote add origirin https://github.com/ogisetiawan/simple-snippet.git 
+   * ? menggunakan https;
+   * * git remote add origirin git@github.com:ogisetiawan/simple-snippet.git
+   * ? menggunakan ssh;
+   * todo: perbedaaan https dan ssh ssh tidak perlu login kembali
+   */
+
+    /**
+    * !### GIT MANUALY ###
+    * * git init 
+    * * git add . 
+    * * git commit -m "first commit" 
+    * * git remote add origin https://github.com/ogisetiawan/simple-snippet.git
+    * * git push -u origin master
+    */
